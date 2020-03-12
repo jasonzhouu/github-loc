1a0016da0d8d1bd8f2ad51c5d467a75ad8e31d63
Yusheng Zhou 周宇盛 <jasonzhouu@gmail.com>
2020-03-12 18:43:23 +0800

    可以没有token

--- getCodeFrequency.js
+++ getCodeFrequency.js
@@ -4,12 +4,19 @@ const calculateLoc = require('./calculateLoc');
 module.exports = function getCodeFrequency(repoName, token) {
   const url = `https://api.github.com/repos/${repoName}/stats/code_frequency`;
 
-  return axios.get(url, {
-    // 参考：https://flaviocopes.com/axios-send-authorization-header/
-    headers: {
-      Authentication: `token ${token}`,
-    },
-  }).then((stat) => calculateLoc(stat.data))
+  let axiosPromise;
+  if (token !== undefined) {
+    axiosPromise = axios.get(url, {
+      // 参考：https://flaviocopes.com/axios-send-authorization-header/
+      headers: {
+        Authentication: `token ${token}`,
+      },
+    });
+  } else {
+    axiosPromise = axios.get(url);
+  }
+
+  return axiosPromise.then((stat) => calculateLoc(stat.data))
     .catch((error) => {
       if (error) throw error;
     });
--- test/getCodeFrequency.test.js
+++ test/getCodeFrequency.test.js
@@ -1,9 +1,9 @@
 const getCodeFrequency = require('../getCodeFrequency');
 const config = require('./config');
 
-const { repoName, token } = config;
+const { repoName } = config;
 
-getCodeFrequency(repoName, token)
+getCodeFrequency(repoName)
   .then((data) => {
     console.log(data);
   });
